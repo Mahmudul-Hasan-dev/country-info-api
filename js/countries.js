c9e4a8e351ef5c3e8a800573106ee8bbfd0580b9
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-04-04 10:15:22 +0600

    update done

--- js/countries.js
+++ js/countries.js
@@ -11,11 +11,11 @@ const displayCountries = (countries) => {
     countries.forEach(country => {
         // console.log(country.languages)
         const countryDiv = document.createElement('div')
-        countryDiv.classlist = 'country'
+        countryDiv.classList = "country"
         countryDiv.innerHTML = `<h3>Name :${country.name.common}</h3>
         <h5>Capital: ${country.capital ? country.capital[0] : "no capital"}</h5>
         <h5>Continents :${country.continents}</h5>
-        <h5>Flags :${country.flag}</h5>`
+        <h1>Flags :${country.flag}</h1>`
 
         countryContainer.appendChild(countryDiv)
 
